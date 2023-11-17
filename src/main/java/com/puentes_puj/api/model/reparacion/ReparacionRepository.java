12fa68cbf7bd28b03545d85b45a97b06abb8a823
Daniel Vargas <dvargasp@javeriana.edu.co>
2023-11-17 02:24:05 -0500

    Create ReparacionRepository.java

---
+++ src/main/java/com/puentes_puj/api/model/reparacion/ReparacionRepository.java
@@ -0,0 +1,7 @@
+package com.puentes_puj.api.model.reparacion;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ReparacionRepository extends CrudRepository<Reparacion, Long> {
+    
+}
