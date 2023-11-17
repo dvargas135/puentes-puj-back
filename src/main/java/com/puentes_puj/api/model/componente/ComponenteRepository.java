8eff2efaca92392a6e0b8d2b36f3783c8ef7873b
Daniel Vargas <dvargasp@javeriana.edu.co>
2023-11-17 02:23:40 -0500

    Create ComponenteRepository.java

---
+++ src/main/java/com/puentes_puj/api/model/componente/ComponenteRepository.java
@@ -0,0 +1,7 @@
+package com.puentes_puj.api.model.componente;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ComponenteRepository extends CrudRepository<Componente, Long> {
+    
+}
